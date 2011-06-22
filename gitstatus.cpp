958ca09edf2927beeb2bdf6f99776fdd7b34be7d
Aaron McDaid <aaronmcdaid@gmail.com>
2011-06-23 00:43:07 +0100

    Remove gitstatus.?pp and comment.txt

--- gitstatus.cpp
+++
@@ -1,8 +0,0 @@
-#include "gitstatus.hpp"
-const char gitstatus_[] = 
-#include "comment.txt"
-"<gitstatus.txt>\n"
-#include "gitstatus.txt"
-"</gitstatus.txt>\n"
-;
-const char * gitstatus = gitstatus_;
