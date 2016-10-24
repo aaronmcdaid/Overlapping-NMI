c4a00257fcf4c31324ec3e93c14d0ea26bf69c28
Aaron McDaid <aaronmcdaid@gmail.com>
2016-10-24 14:34:26 +0200

    deleting more stuff

--- aaron_utils.hpp
+++ aaron_utils.hpp
@@ -50,60 +50,11 @@ enum {
 #define orDie(x ,errorCond ,msg) ({ typeof(x) y = (x); y!=errorCond || Die(msg); y; })
 #define pipe_orDie(filedes)            orDie(pipe(filedes) ,-1 ,"Couldn't pipe(2)")
 #define fork_orDie()                   orDie(fork()        ,-1 ,"Couldn't fork(2)")
-#define orDie1(x) orDie(x, -1, "died @ " __FILE__ ":" TOSTRING(__LINE__) "\t(" #x ")")
-#define orExitChild(x) ({ typeof(x) y = (x); if (testForError(y)) exit(EXIT_CHILD); y; })
-
-int testForError(FILE * x);
-int testForError(int x);
-
-std::string show(int64 x);
-std::string show(const char * x);
-std::string show(const std::string &x);
 
 #define unless(x) if(!(x))
 
-
-struct DummyOutputStream {
-	DummyOutputStream& operator << (int);
-	DummyOutputStream& operator << (const char*);
-};
-extern DummyOutputStream dummyOutputStream;
-
-#define assertEQint(x,y) do { unless((x)==(y)) { PP(x); PP(y); } assert((x)==(y)); } while(0)
 #define DYINGWORDS(x) for (int klsdjfslkfj = (x) ? 0 : 1; klsdjfslkfj!=0; klsdjfslkfj--, ({ assert (x); }) )
-#define UNUSED __attribute__ ((__unused__))
-
-namespace amd {
-
-istream *zcatThis(const char *gzippedFile);
-bool fileExists(const char *);
-
-
-struct NotImplemented {
-};
-
-struct FormatFlagStack {
-	std :: vector< ios_base :: fmtflags> the_stack;
-	std :: vector< std      :: streamsize > the_stack_of_precision; // setprecision(...)
-	struct PushT {
-		FormatFlagStack * const parent_stack;
-		PushT (FormatFlagStack *parent_stack_);
-	} push;
-	struct PopT {
-		FormatFlagStack * const parent_stack;
-		PopT  (FormatFlagStack *parent_stack_);
-	} pop;
-	FormatFlagStack();
-	void do_push(ostream &str);
-	void do_pop(ostream &str);
-};
-ostream & operator<< (ostream &str, const FormatFlagStack :: PushT & pusher);
-ostream & operator<< (ostream &str, const FormatFlagStack :: PopT  & pusher);
-
-} // namespace amd
 
 #define VERYCLOSE(a,b) (1e-10 > fabs((a)-(b)))
-
-#define printfstring(...) ({ char str[1000]; sprintf(str, __VA_ARGS__) ; (std::string (str)); })
 
 #endif
